61ba3a1833464bf379a1ac77e18be9190281713d
lassenextkbh <lass365f@edu.next.kbh.dk>
2024-05-17 01:51:07 +0200

    Movie List and Movie Card Components + Hover Effect & Movies Endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { getSession } from "next-auth/react";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,12 +25,17 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+
   const { data: user } = useCurrentUser();
 
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Dagens bedste forslag til dig" data={movies} />
+      </div>
     </>
   );
 }
